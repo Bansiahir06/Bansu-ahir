b99d65ad05a2157216c3887dada1e337f961913b
Bansiahir06 <shantiahir14.gmaip.com@gmail.com>
2026-01-08 10:30:02 +0000

    scopp

--- scope.js
+++ scope.js
@@ -1,11 +1,17 @@
 
-
+let a =300
 if(true){
     let a =10
     const b =20
-    var c =30
-}
+    console.log("INNER:",a)
 
-// console.log(a);
+}
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+    
+// }
+
+
+console.log(a);
 // console.log(b)
-console.log(c)
+// console.log(c)
