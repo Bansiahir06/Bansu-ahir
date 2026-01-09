48f0b54c5f2c25537bf3e246a0019b8888cf529e
Bansiahir06 <shantiahir14.gmaip.com@gmail.com>
2026-01-09 11:38:44 +0000

    obj2

---
+++ obj2.js
@@ -0,0 +1,27 @@
+// const myObject = {
+//     js: 'Javascript',
+//     cpp: 'c++',
+//     rb: 'ruby',
+//     swift: 'swift by apple'
+// };
+
+// for (const key in myObject) {
+//     // console.log(myObject[key]);
+// }
+
+// const programming =["js","rb","py","java","cpp"]
+
+// for (const key of programming) {
+//     console.log(key);
+// }
+
+// const map=new Map()
+// map.set('IN',"india");
+// map.set('US',"united state of america");
+// map.set('Fr',"France");
+// map.set('IN',"india");
+
+// for (const key of map) {
+//     console.log(key);
+// }
+
